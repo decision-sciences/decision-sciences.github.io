e98db593432b1f7e7e5f0dbb2d38ed01e4672a12
nposner12 <52418497+nposner12@users.noreply.github.com>
2020-06-10 18:24:12 -0400

    CAA

---
+++ CAA app/review/screens/d12245cc-1680-458d-89dd-4f0d7fb22724.js
@@ -0,0 +1,52 @@
+var content='<div class="ui-page" deviceName="iPhoneX" deviceType="mobile" deviceWidth="375" deviceHeight="812">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devMobile devIOS canvas firer commentable non-processed" alignment="left" name="Template 1" width="375" height="812">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1591825506981.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1591825506981-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1591825506981-ie8.css" /><![endif]-->\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+    <div id="s-d12245cc-1680-458d-89dd-4f0d7fb22724" class="screen growth-vertical devMobile devIOS canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="Screen 1" width="375" height="812">\
+    <div id="backgroundBox"></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1591825506981.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1591825506981-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/screens/d12245cc-1680-458d-89dd-4f0d7fb22724-1591825506981-ie8.css" /><![endif]-->\
+      <div id="s-Paragraph_1" class="pie richtext autofit firer ie-background commentable non-processed"   datasizewidth="280px" datasizeheight="67px" dataX="51" dataY="265" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_1_0">Welcome to the Google COVID-19 App</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+\
+      <div id="s-Image_1" class="pie image firer ie-background commentable non-processed"   datasizewidth="126px" datasizeheight="100px" dataX="127" dataY="128"   alt="image">\
+          <img src="./images/89925c88-36df-46be-b34f-65ef9ddc33a7.PNG" />\
+      </div>\
+      <div id="s-Paragraph_2" class="pie richtext autofit firer ie-background commentable non-processed"   datasizewidth="311px" datasizeheight="60px" dataX="37" dataY="363" >\
+        <div class="backgroundLayer"></div>\
+        <div class="paddingLayer">\
+          <div class="clipping">\
+            <div class="content">\
+              <div class="valign">\
+                <span id="rtr-s-Paragraph_2_0">This app is designed to notify if you have been exposed to someone who has tested positive for COVID-19.</span>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      <div id="s-Button" class="pie percentage label singleline firer click commentable pin hpin-center non-processed-percentage non-processed-pin non-processed"   datasizewidth="95%" datasizeheight="57px" dataX="1" dataY="648" ><div class="backgroundLayer"></div><div class="paddingLayer"><div class="content"><div class="valign"><span id="rtr-s-Button_0">Get Started</span></div></div></div></div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
